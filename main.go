b7e9e60899e8cc44221a67322513bce0b9e4a96d
EIMIKI <kou.103.kan@gmail.com>
2018-05-25 09:59:13 +0900

    openDB残ってたので削除

--- main.go
+++ main.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-	"os"
 	"simpleTodo/controller"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-func openDB() *sql.DB {
-	dbuser := os.Getenv("DBUSER")
-	dbpass := os.Getenv("DBPASS")
-	db, err := sql.Open("mysql", ""+dbuser+":"+dbpass+"@/todos")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return db
-}
 
 func main() {
 	r := controller.Init()
