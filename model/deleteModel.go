f4619e01aa5c3fad973d687439f739b3009f946e
EIMIKI <kou.103.kan@gmail.com>
2018-05-27 17:53:00 +0900

    idがあれば削除は可能なのでnameを削除

--- model/deleteModel.go
+++ model/deleteModel.go
@@ -14,11 +14,10 @@ type DelTodo struct { //削除用
 func DeleteTodo(delTodo DelTodo, cookie objx.Map) {
 	db := openDB()
 	defer db.Close()
-	name := cookie["Name"].(string)
 
 	if delTodo.Delete != nil {
 		for _, v := range delTodo.Delete {
-			_, err := db.Exec("delete from todo where todoid=" + strconv.Itoa(v) + "and user='" + name + "';")
+			_, err := db.Exec("delete from todo where todoid=" + strconv.Itoa(v) + ";")
 			if err != nil {
 				log.Fatalln(err)
 			}
