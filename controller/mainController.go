ca4710e0d6feee72564c9cffc9eaa36f6e83735d
EIMIKI <kou.103.kan@gmail.com>
2018-05-27 16:31:12 +0900

    ユーザを一意に決定できる値を取得するように変更

--- controller/mainController.go
+++ controller/mainController.go
@@ -52,7 +52,7 @@ func Callback(c *gin.Context) {
 	}
 
 	cookieValue := objx.New(map[string]interface{}{
-		"Name": user.Name(),
+		"Id": user.AuthCode(),
 	}).MustBase64()
 
 	//c.SetCookie(name, value, maxAge, path, domain, secure, httpOnly)
